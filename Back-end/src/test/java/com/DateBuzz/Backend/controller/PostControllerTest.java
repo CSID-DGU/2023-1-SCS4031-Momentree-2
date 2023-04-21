b027e60ffe524e926acd19c8cdeeb5bab984922b
shoeone96 <dlwnddnjs96@gmail.com>
2023-04-21 15:51:58 +0900

    test : PostController 테스트 세팅

---
+++ Back-end/src/test/java/com/DateBuzz/Backend/controller/PostControllerTest.java
@@ -0,0 +1,30 @@
+package com.DateBuzz.Backend.controller;
+
+import com.DateBuzz.Backend.service.PostService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void 게시글_목록_받아오기(){
+
+    }
+
+
+}
