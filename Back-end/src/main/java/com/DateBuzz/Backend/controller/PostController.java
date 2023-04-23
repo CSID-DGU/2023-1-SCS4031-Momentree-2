a5ba5f31f7c29b0d1cf81db68dee138c25ad2f7c
shoeone96 <dlwnddnjs96@gmail.com>
2023-04-23 16:22:30 +0900

    feat(controller): PostController 내부 기능 구현

--- Back-end/src/main/java/com/DateBuzz/Backend/controller/PostController.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/controller/PostController.java
@@ -1,7 +1,10 @@
 package com.DateBuzz.Backend.controller;
 
+import com.DateBuzz.Backend.controller.responseDto.RecordResponseDto;
 import com.DateBuzz.Backend.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,8 @@ public class PostController {
 
     private final PostService postService;
 
-
+    @GetMapping("/community")
+    private Page<RecordResponseDto> list(Pageable pageable){
+        return postService.getList(pageable);
+    }
 }
