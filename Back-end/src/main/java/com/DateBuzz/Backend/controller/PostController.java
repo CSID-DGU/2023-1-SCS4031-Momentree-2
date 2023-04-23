b36a3de14caeb4ccdab0af1bd07881bfbeeebd9e
shoeone96 <dlwnddnjs96@gmail.com>
2023-04-23 13:49:36 +0900

    test(PostControllerTest): 게시글 목록 받아오기 test작성

---
+++ Back-end/src/main/java/com/DateBuzz/Backend/controller/PostController.java
@@ -0,0 +1,15 @@
+package com.DateBuzz.Backend.controller;
+
+import com.DateBuzz.Backend.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+
+}
--- Back-end/src/test/java/com/DateBuzz/Backend/controller/PostControllerTest.java
+++ Back-end/src/test/java/com/DateBuzz/Backend/controller/PostControllerTest.java
@@ -6,7 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,8 +27,14 @@ public class PostControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void 게시글_목록_받아오기(){
+    void 게시글_목록_받아오기() throws Exception{
 
+        //Todo: mocking 데이터 삽입
+
+        mockMvc.perform(get("/community")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
 
