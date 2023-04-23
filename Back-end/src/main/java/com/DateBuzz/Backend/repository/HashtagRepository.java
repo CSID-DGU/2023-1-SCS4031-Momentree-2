227cdcfa8d3a815b73570e5910be3a193ccf0991
shoeone96 <dlwnddnjs96@gmail.com>
2023-04-23 18:01:17 +0900

    feature(repository): 해시태그 정보 저장을 위한 HashtagRepository 생성

---
+++ Back-end/src/main/java/com/DateBuzz/Backend/repository/HashtagRepository.java
@@ -0,0 +1,6 @@
+package com.DateBuzz.Backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashtagRepository extends JpaRepository<HashtagRepository, Long> {
+}
