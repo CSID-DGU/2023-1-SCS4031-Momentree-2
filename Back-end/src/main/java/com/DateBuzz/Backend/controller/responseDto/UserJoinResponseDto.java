f151269f35eb9ee2fe994c3f71ddf27dfb83d2f9
shoeone96 <dlwnddnjs96@gmail.com>
2023-04-26 16:08:41 +0900

    feature(responseDto): 회원가입 완료 후 전달할 정보 작성

--- Back-end/src/main/java/com/DateBuzz/Backend/controller/responseDto/UserJoinResponseDto.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/controller/responseDto/UserJoinResponseDto.java
@@ -1,4 +1,20 @@
 package com.DateBuzz.Backend.controller.responseDto;
 
+import com.DateBuzz.Backend.model.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserJoinResponseDto {
+
+    private String userName;
+    private String nickName;
+
+    public static UserJoinResponseDto fromUser(UserEntity user){
+        return new UserJoinResponseDto(
+                user.getUserName(),
+                user.getNickname()
+        );
+    }
 }
