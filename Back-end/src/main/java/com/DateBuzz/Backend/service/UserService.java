2ea30eb7673cb9dc6ae627a44baf66e10e691a1b
shoeone96 <dlwnddnjs96@gmail.com>
2023-04-26 16:12:38 +0900

    feature(service): 휘원가입 로직 작성

--- Back-end/src/main/java/com/DateBuzz/Backend/service/UserService.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/service/UserService.java
@@ -2,12 +2,40 @@ package com.DateBuzz.Backend.service;
 
 import com.DateBuzz.Backend.controller.requestDto.UserJoinRequestDto;
 import com.DateBuzz.Backend.controller.responseDto.UserJoinResponseDto;
+import com.DateBuzz.Backend.exception.DateBuzzException;
+import com.DateBuzz.Backend.exception.ErrorCode;
+import com.DateBuzz.Backend.model.entity.UserEntity;
+import com.DateBuzz.Backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder; // encoder 추가
+
+
     public UserJoinResponseDto join(UserJoinRequestDto requestDto) {
-        return new UserJoinResponseDto();
+        //Todo: id 중복 체크
+        userRepository.findByUserName(requestDto.getUserName())
+                .ifPresent(it -> {
+                    throw new DateBuzzException(ErrorCode.DUPLICATED_NAME, String.format("%s 는 이미 존재하는 아이디입니다.", requestDto.getUserName()));
+                });
+        //Todo: nickName 중복 체크
+        userRepository.findByNickname(requestDto.getNickname())
+                .ifPresent(it -> {
+                    throw new DateBuzzException(ErrorCode.DUPLICATED_NAME, String.format("%s 는 이미 존재하는 닉네임입니다.", requestDto.getUserName()));
+                });
+        //Todo: 아이디 비밀번호 정규식 체크
+
+        UserEntity user = UserEntity.fromJoinRequestDto(
+                requestDto.getUserName(),
+                encoder.encode(requestDto.getPassword()),
+                requestDto.getNickname(),
+                requestDto.getEmail());
+        return UserJoinResponseDto.fromUser(userRepository.save(user));
     }
 }
