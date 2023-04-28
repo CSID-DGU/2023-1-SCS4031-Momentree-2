b1940e0bc78a4fcd44729890d4014f0695039cca
banban555 <ban_ban_55@naver.com>
2023-04-28 14:46:58 +0900

    recommend:feature/Add calculate similarity

--- Recommendation/mood_activity.py
+++ Recommendation/mood_activity.py
@@ -2,13 +2,21 @@ import os
 import re
 import pandas as pd
 import numpy as np
-
+import pprint
 
 from tqdm import tqdm
 from tabulate import tabulate
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics.pairwise import cosine_similarity
+
+#유사도를 검사하는 함수
+def cos_sim_matrix(a,b):
+    cos_sin = cosine_similarity(a,b)
+    result_df = pd.DataFrame(data=cos_sin, index=[a.index])
+
+    return result_df
 
 #print를 위한 함수
 def print_dictionary(dic_name):
@@ -25,7 +33,7 @@ def remove_similar_item(list):
         else:
             # 앞서 추가된 요소들과 앞 세 글자가 겹치지 않으면 추가
             for j in range(len(new_list)):
-                if new_list[j][:3] == list[i][:3]:
+                if new_list[j][:1] == list[i][:1]:
                     break
             else:
                 new_list.append(list[i])
@@ -46,7 +54,8 @@ static_tag_df['mood_activity'] = static_tag_df.apply(lambda x: ', '.join([x['moo
 static_tag_df = static_tag_df.drop(['mood', 'activity'], axis=1)
 #한글이 아닌 문자들이 들어가있는 경우 전처리
 static_tag_df['mood_activity'] = static_tag_df['mood_activity'].apply(lambda x: re.sub('[^가-힣\s]', '', x))
-#print(static_tag_df)
+#print (tabulate(static_tag_df, headers='keys', tablefmt='psql', showindex=False))
+
 
 #----------------03. 게시글에 사용된 태그 목록 생성--------------------
 total_count = len(static_tag_df)
@@ -56,6 +65,7 @@ for tags in static_tag_df['mood_activity']:
     static_tag_list = list(set(static_tag_list))
 static_tag_list = remove_similar_item(static_tag_list)
 #print(static_tag_list)
+
 
 #--------------------04. 단어의 가중치 파악--------------------
 tag_count = dict.fromkeys(static_tag_list)
@@ -70,21 +80,32 @@ for each_static_tag in static_tag_df['mood_activity']:
         else:
             continue
 #print_dictionary(tag_count)
-
-# #빈도수를 바탕으로 단어의 가중치 계산
 for each_static_tag in tag_count:
     tag_count[each_static_tag] = np.log10(total_count/tag_count[each_static_tag])
-#print_dictionary(tag_count)
 
+
+#--------------------05. 단어의 가중치를 반영하여 게시글 표현--------------------
 #태그 가중치를 반영한 태그 representation
 static_tag_representation = pd.DataFrame(columns=sorted(static_tag_list), index=static_tag_df.index)
 #print(static_tag_representation)
-for index, each_row in tqdm(static_tag_df.iterrows()):
+for index, each_row in tqdm(static_tag_df.iterrows(), disable=True):
     dict_temp = {} 
+    #static_tag_df에 있는 행들을 순회하면서 공백을 기준으로 모든 태그를 탐색함
     for tag in each_row['mood_activity'].split(' '):
+        #tag리스트에 있는 tag의 목록인지 확인 후 dict_temp에 담아둠
         if tag in tag_count:
             dict_temp[tag] = tag_count[tag]
     row_to_add = pd.DataFrame(dict_temp, index=[index])
+    #print (tabulate(row_to_add, headers='keys', tablefmt='psql'))
     static_tag_representation.update(row_to_add)
-print(static_tag_representation)
-#print(tabulate(static_tag_representation, tablefmt='fancy_outline'))
+#print(tabulate(static_tag_representation.loc[0].to_frame(), headers='keys', tablefmt='psql', floatfmt=".6f"))
+
+#--------------------06. 해당 표를 기반으로 사용자가 좋아요/북마크 게시글과 유사도 판별--------------------
+static_tag_representation = static_tag_representation.fillna(0)
+#print(tabulate(static_tag_representation.loc[0].to_frame(), headers='keys', tablefmt='psql', floatfmt=".6f"))
+
+cs_df = cos_sim_matrix(static_tag_representation, static_tag_representation)
+#print(cs_df.head())
+print(cs_df[0].sort_values(ascending=False))
+
+#--------------------07. 유사도가 높은 것을 기준으로 추천 리스트 생성--------------------
