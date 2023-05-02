c8d992f0cd8ce3a24a7b5c680fbc61e86cccf8b9
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-02 17:22:53 +0900

    refactor(record): 이미지 저장 테이블 별도 분리로 인해 코드 수정

--- Back-end/src/main/java/com/DateBuzz/Backend/controller/responseDto/RecordedPlaceResponseDto.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/controller/responseDto/RecordedPlaceResponseDto.java
@@ -1,8 +1,11 @@
 package com.DateBuzz.Backend.controller.responseDto;
 
+import com.DateBuzz.Backend.model.entity.PlaceImgEntity;
 import com.DateBuzz.Backend.model.entity.RecordedPlaceEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -12,13 +15,15 @@ public class RecordedPlaceResponseDto {
     private Long recordId;
     private int order;
     private String placeName;
+    private List<PlaceImgResponseDto> placeImages;
 
-    public static RecordedPlaceResponseDto fromRecordedPlace(RecordedPlaceEntity recordedPlace){
+    public static RecordedPlaceResponseDto fromRecordedPlace(RecordedPlaceEntity recordedPlace, List<PlaceImgResponseDto> images){
         return new RecordedPlaceResponseDto(
                 recordedPlace.getId(),
                 recordedPlace.getRecord().getId(),
                 recordedPlace.getOrders(),
-                recordedPlace.getPlaceName()
+                recordedPlace.getPlaceName(),
+                images
         );
     }
 }
