18afbb711c7d2db5455a3323dca5827235008015
banban555 <ban_ban_55@naver.com>
2023-05-03 09:20:28 +0900

    feature:delete duplicate

--- Recommendation/mood_activity.py
+++ Recommendation/mood_activity.py
@@ -1,4 +1,3 @@
-import os
 import re
 import pandas as pd
 import numpy as np
@@ -46,7 +45,7 @@ def remove_similar_item(list):
 conn = pymysql.connect(host='127.0.0.1', user='root', password='1234', db='pythonDB', charset='utf8')
 cur = conn.cursor()
 
-#--------------------Data 가져오기-------------------
+#--------------------hastag Data 가져오기-------------------
 sql = 'select * from hastag'
 cur.execute(sql)
 res = cur.fetchall()
@@ -58,7 +57,7 @@ prototypeData = pd.DataFrame.from_records(res, columns=[desc[0] for desc in cur.
 course_hastag = prototypeData[['course_id', 'tag_name']]
 course_hastag = course_hastag.groupby('course_id')['tag_name'].apply(lambda x: ' '.join(x)).reset_index()
 
-#2. 한글이 아닌 값이나 중복되는 값 삭제
+#2. 한글이 아니거나 값이나 중복되는 값 삭제
 course_hastag['tag_name'] = course_hastag['tag_name'].apply(lambda x: re.sub('[^가-힣\s]', '', x))
 #print (tabulate(course_hastag, headers='keys', tablefmt='psql', showindex=False))
 
@@ -72,7 +71,7 @@ for tags in course_hastag['tag_name']:
     hastag_list = remove_similar_item(hastag_list)
 #print(hastag_list)
 
-#--------------------04. 단어의 가중치 파악--------------------
+#--------------------04. 해시태그 별 가중치 파악--------------------
 hastag_count = dict.fromkeys(hastag_list)
 for each_hastag in course_hastag['tag_name']:
     for tag in each_hastag.split(' '):
@@ -84,15 +83,12 @@ for each_hastag in course_hastag['tag_name']:
                 hastag_count[tag] = hastag_count[tag]+1 
         else:
             continue
-
-# #--------------------05. 단어의 가중치 계산--------------------
 for each_hastag in hastag_count:
     hastag_count[each_hastag] = np.log10(num_of_course/hastag_count[each_hastag])
 #print_dictionary(hastag_count)
 
 
-# #--------------------05. 가중치를 반영하여 게시글 표현--------------------
-# #태그 가중치를 반영한 태그 representation
+# #--------------------05. 해시태그 가중치를 바탕으로 게시글 표현--------------------
 hastag_representation = pd.DataFrame(columns=sorted(hastag_list), index=course_hastag.index)
 #print(hastag_representation)
 for index, each_row in tqdm(course_hastag.iterrows(), disable=True):
@@ -105,13 +101,12 @@ for index, each_row in tqdm(course_hastag.iterrows(), disable=True):
     row_to_add = pd.DataFrame(dict_temp, index=[index])
     hastag_representation.update(row_to_add)
 #print(tabulate(hastag_representation.loc[0].to_frame(), headers='keys', tablefmt='psql', floatfmt=".6f"))
-
-hastag_representation = hastag_representation.fillna(0)
+hastag_representation = hastag_representation.fillna(0) #NAN값을 0으로 변경
 hastag_similarity = cos_sim_matrix(hastag_representation, hastag_representation)
 #print(tabulate(hastag_similarity, headers='keys', tablefmt='psql', floatfmt=".6f"))
 
 
-# --------------------06. 해당 표를 기반으로 사용자가 좋아요/북마크 게시글과 유사도 판별--------------------
+# --------------------06. 특정 사용자가 북마크 좋아요 한 record data가져오기 --------------------
 # bookmarkd와 like DB에서 record_id 가져오기
 user_id = 3;
 sql = 'select user_id, record_id from bookmark where user_id = %s'
@@ -124,7 +119,7 @@ cur.execute(sql, (user_id))
 like_df = pd.DataFrame(cur.fetchall(), columns=['user_id', 'record_id'])
 #print(tabulate(like_df, headers='keys', tablefmt='psql', showindex=False))
 
-# 두 데이터프레임을 합치기
+# 두 데이터프레임을 합치고 중복제거
 user_df = pd.concat([bookmark_df, like_df])
 user_df.drop_duplicates(inplace=True)
 #print(tabulate(user_df, headers='keys', tablefmt='psql', showindex=False))
@@ -138,11 +133,13 @@ for idx in user_similarities.index:  # 각 행에 대해 반복
     user_similarities.loc[idx, col] = 0  # 인덱스 값과 같은 열에 해당하는 값을 0으로 설정
 #print(tabulate(user_similarities, headers='keys', tablefmt='psql', floatfmt=".3f"))
 
+#recommend에 추천 아이템 5개가 쌓일 때까지 가장 높은 항목을 추출함.
 recommended = []
 while len(recommended) < 5:
-    top_similarities = user_similarities.stack().nlargest(6)  # 유사도가 가장 높은 6개 항목을 추출 (자기 자신 제외)
-    top_similarities = top_similarities[~top_similarities.index.isin(recommended)]  # 추천 목록에 이미 존재하는 항목 제외
-    recommended.extend(top_similarities.index.get_level_values(1).tolist())  # 추천 목록에 추가
+    top_similarities = user_similarities.stack().nlargest(1)  # 유사도가 가장 높은 항목을 추출
+    print(top_similarities)
+    if top_similarities.index[0][1] not in recommended:  # 이미 추천된 항목이 아니라면
+        recommended.append(top_similarities.index[0][1])  # 추천 목록에 추가
     user_similarities.loc[top_similarities.index] = 0  # 추천 목록에 있는 게시글과 유사도를 0으로 만들어서 중복 추천 방지
 
 print(recommended)
