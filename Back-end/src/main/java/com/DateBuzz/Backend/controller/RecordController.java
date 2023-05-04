daf4e6cd22964644424191d245076ba979d0cb86
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-04 11:56:28 +0900

    feat(controller): user 정보 반환 paging 설정 추가, 사용 안하는 예외처리 삭제

--- Back-end/src/main/java/com/DateBuzz/Backend/controller/RecordController.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/controller/RecordController.java
@@ -7,6 +7,7 @@ import com.DateBuzz.Backend.service.RecordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +21,12 @@ public class RecordController {
     private final RecordService recordService;
 
     @GetMapping("/community")
-    private Response<Page<RecordResponseDto>> list(Pageable pageable){
+    private Response<Page<RecordResponseDto>> list(@PageableDefault(size=3) Pageable pageable){
         return Response.success(recordService.getList(pageable));
     }
 
     @PostMapping("/community")
-    private Response<Void> records(@RequestBody RecordRequestDto requestDto, Authentication authentication) throws Exception {
+    private Response<Void> records(@RequestBody RecordRequestDto requestDto, Authentication authentication){
         recordService.writes(requestDto, authentication.getName());
         return Response.success();
     }
