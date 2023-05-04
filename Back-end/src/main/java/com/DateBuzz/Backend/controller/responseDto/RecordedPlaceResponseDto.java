c2ed42e16b57ac3ed3bd43f34696ffbac3820054
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-04 11:53:20 +0900

    refactor(responseDto): 장소 좌표 추가

--- Back-end/src/main/java/com/DateBuzz/Backend/controller/responseDto/RecordedPlaceResponseDto.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/controller/responseDto/RecordedPlaceResponseDto.java
@@ -2,6 +2,7 @@ package com.DateBuzz.Backend.controller.responseDto;
 
 import com.DateBuzz.Backend.model.entity.PlaceImgEntity;
 import com.DateBuzz.Backend.model.entity.RecordedPlaceEntity;
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,8 +14,13 @@ public class RecordedPlaceResponseDto {
 
     private Long placeId;
     private Long recordId;
-    private int order;
+    private int orders;
     private String placeName;
+    private String address;
+    private String addressGu;
+    private String addressX;
+    private String addressY;
+    private String placeContent;
     private List<PlaceImgResponseDto> placeImages;
 
     public static RecordedPlaceResponseDto fromRecordedPlace(RecordedPlaceEntity recordedPlace, List<PlaceImgResponseDto> images){
@@ -23,6 +29,11 @@ public class RecordedPlaceResponseDto {
                 recordedPlace.getRecord().getId(),
                 recordedPlace.getOrders(),
                 recordedPlace.getPlaceName(),
+                recordedPlace.getAddress(),
+                recordedPlace.getAddressGu(),
+                recordedPlace.getAddressX(),
+                recordedPlace.getAddressY(),
+                recordedPlace.getPlaceContent(),
                 images
         );
     }
