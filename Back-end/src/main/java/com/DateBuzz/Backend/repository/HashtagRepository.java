e3501e57f0d6aa35c864400bcf8f1c52cf188b52
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-08 12:54:54 +0900

    fix(repository): hashtag 반환 시 빈 배열 전송 문제 해결 위한 탐색 메서드 작성

--- Back-end/src/main/java/com/DateBuzz/Backend/repository/HashtagRepository.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/repository/HashtagRepository.java
@@ -1,9 +1,14 @@
 package com.DateBuzz.Backend.repository;
 
 import com.DateBuzz.Backend.model.entity.HashtagEntity;
+import com.DateBuzz.Backend.model.entity.HashtagType;
+import com.DateBuzz.Backend.model.entity.RecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HashtagRepository extends JpaRepository<HashtagEntity, Long> {
+    List<HashtagEntity> findAllByRecordAndHashtagType(RecordEntity record, HashtagType hashtagType);
 }
