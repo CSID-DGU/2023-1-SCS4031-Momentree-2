d27c0d9ac65fca47bdddf709a551430dd834c3b1
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-20 17:46:27 +0900

    feat(repository): 북마크 데이터 접근을 위한 repository 작성

---
+++ Back-end/src/main/java/com/DateBuzz/Backend/repository/BookmarkRepository.java
@@ -0,0 +1,16 @@
+package com.DateBuzz.Backend.repository;
+
+import com.DateBuzz.Backend.model.entity.BookmarkEntity;
+import com.DateBuzz.Backend.model.entity.RecordEntity;
+import com.DateBuzz.Backend.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface BookmarkRepository extends JpaRepository<BookmarkEntity, Long> {
+    Optional<BookmarkEntity> findByUserAndRecord(UserEntity user, RecordEntity record);
+    @Query(value = "select count(*) from BookmarkEntity entity where entity.record = :record and entity.bookmarkStatus = 1")
+    Integer countByRecord(@Param("record") RecordEntity record);
+}
