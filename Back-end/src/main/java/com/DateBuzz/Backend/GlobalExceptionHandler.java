65ae72faecc725397a2561976a35de02f98cefdd
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-23 23:28:23 +0900

    refactor(exception): 에러 발생 시 클라이언트에 전달하는 전역 클래스도 ErrorResponse 기반의 http status와 메시지 전달하도록 설정

--- Back-end/src/main/java/com/DateBuzz/Backend/GlobalExceptionHandler.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/GlobalExceptionHandler.java
@@ -1,19 +1,21 @@
 package com.DateBuzz.Backend;
 
+import com.DateBuzz.Backend.controller.responseDto.ErrorResponse;
 import com.DateBuzz.Backend.exception.DateBuzzException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DateBuzzException.class)
-    public ResponseEntity<String> handleMyException(DateBuzzException ex) {
+    @ResponseBody
+    public ErrorResponse<HttpStatus> handleMyException(DateBuzzException ex) {
         String errorMessage = ex.getErrorCode().getMessage();
         HttpStatus status = ex.getErrorCode().getStatus();
-        return new ResponseEntity<>(errorMessage, status);
+        return ErrorResponse.error(errorMessage, status);
     }
 }
