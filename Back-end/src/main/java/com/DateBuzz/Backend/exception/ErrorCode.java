01e3c80a7f5ff92a5fc1d522a9004829c9224b11
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-23 23:27:23 +0900

    feat(exception): 예외 발생 상황 추가(추천 파이썬 파일 실행, 이미지 업로드 문제)

--- Back-end/src/main/java/com/DateBuzz/Backend/exception/ErrorCode.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/exception/ErrorCode.java
@@ -12,7 +12,10 @@ public enum ErrorCode {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
     DATE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시글입니다."),
-    INVALID_USER(HttpStatus.UNAUTHORIZED, "해당 유저가 작성한 게시물이 아닙니다.");
+    INVALID_USER(HttpStatus.UNAUTHORIZED, "해당 유저가 작성한 게시물이 아닙니다."),
+    S3_UPLOAD_PROBLEM(HttpStatus.CONFLICT, "이미지 업로드 중 문제가 발생했습니다."),
+    PYTHON_READING_PROBLEM(HttpStatus.CONFLICT, "파이썬 파일 확인 중 문제가 발생했습니다."),
+    PATH_PROBLEM(HttpStatus.CONFLICT, "파이썬 파일의 경로가 잘못되었습니다.");
     private HttpStatus status;
     private String message;
 }
