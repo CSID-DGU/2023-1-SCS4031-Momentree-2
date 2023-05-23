863e04dc0ad14b2e2bef20a174f7c07999def70c
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-24 02:09:52 +0900

    feat(controller): 유저 정보 반환하는 api

--- Back-end/src/main/java/com/DateBuzz/Backend/controller/UserController.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/controller/UserController.java
@@ -3,10 +3,13 @@ package com.DateBuzz.Backend.controller;
 import com.DateBuzz.Backend.controller.requestDto.UserJoinRequestDto;
 import com.DateBuzz.Backend.controller.requestDto.UserLoginRequestDto;
 import com.DateBuzz.Backend.controller.responseDto.Response;
+import com.DateBuzz.Backend.controller.responseDto.UserInfoResponseDto;
 import com.DateBuzz.Backend.controller.responseDto.UserJoinResponseDto;
 import com.DateBuzz.Backend.controller.responseDto.UserLoginResponseDto;
 import com.DateBuzz.Backend.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,10 @@ public class UserController {
     public Response<UserLoginResponseDto> join(@RequestBody UserLoginRequestDto requestDto){
         String token = userService.login(requestDto);
         return Response.success(new UserLoginResponseDto(token));
+    }
+
+    @GetMapping("/UserInfo")
+    public Response<UserInfoResponseDto> getUserInfo(Authentication authentication){
+        return Response.success(userService.getInfo(authentication.getName()));
     }
 }
