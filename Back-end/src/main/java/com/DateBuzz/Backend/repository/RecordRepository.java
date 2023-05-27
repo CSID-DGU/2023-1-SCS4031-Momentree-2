888d9079490580b95d8ea3c44bea68e65a10e16f
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-28 02:40:55 +0900

    feat(repository): 수정할 게시글을 찾는 메서드 추가

--- Back-end/src/main/java/com/DateBuzz/Backend/repository/RecordRepository.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/repository/RecordRepository.java
@@ -7,10 +7,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RecordRepository extends JpaRepository<RecordEntity, Long> {
     List<RecordEntity> findAllByUser(UserEntity user);
     @Query(value = "select count(*) from RecordEntity record where record.user = :user")
     Integer myRecordCnt(UserEntity user);
+
+    Optional<RecordEntity> findByUserAndId(UserEntity user, Long id);
 }
