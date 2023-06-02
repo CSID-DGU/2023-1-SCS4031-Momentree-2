87611d57e234f02fca6854d2cc2a89ad858a6574
shoeone96 <dlwnddnjs96@gmail.com>
2023-06-02 15:28:52 +0900

    refactor: error case 추가, bookmark 검색 조건 변경

--- Back-end/src/main/java/com/DateBuzz/Backend/exception/ErrorCode.java
+++ Back-end/src/main/java/com/DateBuzz/Backend/exception/ErrorCode.java
@@ -15,7 +15,9 @@ public enum ErrorCode {
     INVALID_USER(HttpStatus.UNAUTHORIZED, "해당 유저가 작성한 게시물이 아닙니다."),
     S3_UPLOAD_PROBLEM(HttpStatus.CONFLICT, "이미지 업로드 중 문제가 발생했습니다."),
     PYTHON_READING_PROBLEM(HttpStatus.CONFLICT, "파이썬 파일 확인 중 문제가 발생했습니다."),
-    PATH_PROBLEM(HttpStatus.CONFLICT, "파이썬 파일의 경로가 잘못되었습니다.");
+    PATH_PROBLEM(HttpStatus.CONFLICT, "파이썬 파일의 경로가 잘못되었습니다."),
+    PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 장소를 찾을 수 없습니다."),
+    MAX_PLACES(HttpStatus.UNAUTHORIZED, "5개 초과의 장소를 등록하실 수 없습니다.");
     private HttpStatus status;
     private String message;
 }
