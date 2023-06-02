e2631c5762e7dbe5f21a8fa34318cc03539b04ec
shoeone96 <dlwnddnjs96@gmail.com>
2023-06-02 15:21:21 +0900

    feat(repository): follow db 접근을 위한 repository 추가

---
+++ Back-end/src/main/java/com/DateBuzz/Backend/repository/FollowRepository.java
@@ -0,0 +1,22 @@
+package com.DateBuzz.Backend.repository;
+
+import com.DateBuzz.Backend.model.entity.FollowEntity;
+import com.DateBuzz.Backend.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FollowRepository extends JpaRepository<FollowEntity, Long> {
+
+    Optional<FollowEntity> findByFollowingAndFollower(UserEntity following, UserEntity follower);
+
+    @Query(value = "select count(*) from FollowEntity entity where entity.following = :user and entity.followStatus = 1")
+    Integer countFollower(@Param("user") UserEntity user);
+
+    @Query(value = "select count(*) from FollowEntity entity where entity.follower = :user and entity.followStatus = 1")
+    Integer countFollowing(@Param("user") UserEntity user);
+}
