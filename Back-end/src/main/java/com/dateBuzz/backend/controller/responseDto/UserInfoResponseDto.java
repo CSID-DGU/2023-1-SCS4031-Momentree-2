99bfb5d9719b881800ea3ea73e6620f569d8eb8b
shoeone96 <dlwnddnjs96@gmail.com>
2023-06-10 13:36:10 +0900

    refactor(responseDto): 기존 userImg 저장 전과 다르게 S3에 저장된 userImg url 전달

--- Back-end/src/main/java/com/dateBuzz/backend/controller/responseDto/UserInfoResponseDto.java
+++ Back-end/src/main/java/com/dateBuzz/backend/controller/responseDto/UserInfoResponseDto.java
@@ -19,6 +19,6 @@ public class UserInfoResponseDto {
                 follower,
                 following,
                 recordCnt,
-                "");
+                user.getProfileImg());
     }
 }
